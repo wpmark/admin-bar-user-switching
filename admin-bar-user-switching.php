4e7939537a2da9a458f91867ffbff5d950160243
Mark Wilkinson <mark@markwilkinson.me>
2014-11-25 21:18:51 +0000

    Amend Function names ready for .org

--- admin-bar-user-switching.php
+++ admin-bar-user-switching.php
@@ -5,15 +5,15 @@ Plugin URI: http://markwilkinson.me
 Description: Building upon the <a href="http://wordpress.org/extend/plugins/user-switching/">User Switching plugin</a> by John Blackbourn this plugin adds a dropdown list of users in the WordPress admin bar with a link to switch to that user, then providing a switch back link in the admin bar too.
 Author: Mark Wilkinson
 Author URI: http://markwilkinson.me
-Version: 1.0
+Version: 0.1
 */
 
 /******************************************************************************************
-* Function mdw_current_url
+* Function abus_current_url
 * Determine the URL of the currently viewed page - will return array if $parse set to true
 * Taken from https://github.com/scottsweb/null/blob/master/functions.php
 ******************************************************************************************/
-function mdw_current_url( $parse = false ) {
+function abus_current_url( $parse = false ) {
 
 	$s = empty( $_SERVER[ 'HTTPS' ] ) ? '' : ( $_SERVER[ 'HTTPS' ] == 'on' ) ? 's' : '';
 	$protocol = substr( strtolower( $_SERVER[ 'SERVER_PROTOCOL' ] ), 0, strpos( strtolower( $_SERVER[ 'SERVER_PROTOCOL' ] ), '/' ) ) . $s;
@@ -28,19 +28,19 @@ function mdw_current_url( $parse = false ) {
 }
 
 /******************************************************************************************
-* Function mdw_usab_initialisation
+* Function abus_usab_initialisation
 * Initialisation plugin to add error message to admin if user switching plugin not present
 ******************************************************************************************/
-function mdw_usab_initialisation() {
-	add_action( 'admin_notices', 'mdw_usab_error' );
+function abus_usab_initialisation() {
+	add_action( 'admin_notices', 'abus_usab_error' );
 }
 
 
 /******************************************************************************************
-* Function mdw_usab_error
+* Function abus_usab_error
 * Deactivates the plugin and throws and error message when User Switching plugin not active
 ******************************************************************************************/
-function mdw_usab_error() {
+function abus_usab_error() {
 
 	if( !class_exists( 'user_switching' ) ) {
 	
@@ -53,14 +53,14 @@ function mdw_usab_error() {
 }
 
 /* start this plugin once all other plugins have loaded */
-add_action( 'plugins_loaded', 'mdw_usab_initialisation' );
+add_action( 'plugins_loaded', 'abus_usab_initialisation' );
 
 /******************************************************************************************
-* Function mdw_user_switching_adminbar
+* Function abus_user_switching_adminbar
 * Adds the Switcht to User menu items in the WordPress admin bar as well as a Switch Back
 * link when users have switched.
 ******************************************************************************************/
-function mdw_user_switching_adminbar() {
+function abus_user_switching_adminbar() {
 	
 	/* include plugin file to make this work on the front end */
 	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
@@ -82,50 +82,50 @@ function mdw_user_switching_adminbar() {
 			
 				/* add admin bar menu for switching to a user */
 				$wp_admin_bar->add_menu( array(
-					'id'    => 'mdw_switch_to_user',
+					'id'    => 'abus_switch_to_user',
 					'title' => 'Switch to User',
 					'href'  => '#',
 				) );
 				
 				/* set some arguments for our user query */
-				$mdw_user_query_args = array(
+				$abus_user_query_args = array(
 					'role' => '',
 					'orderby' => 'display_name'
 				);
 							
 				/* create a new user query */
-				$mdw_user_query = new WP_User_Query( $mdw_user_query_args );
+				$abus_user_query = new WP_User_Query( $abus_user_query_args );
 				
 				/* store results from user query */
-				$mdw_users = $mdw_user_query->get_results();
+				$abus_users = $abus_user_query->get_results();
 				
-				$mdw_current_user = get_current_user_id();
+				$abus_current_user = get_current_user_id();
 				
 				/* check we have users */
-				if( !empty( $mdw_users ) ) {
+				if( !empty( $abus_users ) ) {
 					
 					/* loop through each user */
-					foreach( $mdw_users as $mdw_user ) {
+					foreach( $abus_users as $abus_user ) {
 					
 						/* check whether this user is the current user */
-						if( $mdw_current_user == $mdw_user->ID )
+						if( $abus_current_user == $abus_user->ID )
 							continue;
 						
 						/* get all of this users data */
-						$mdw_user_info = get_userdata( $mdw_user->ID );
+						$abus_user_info = get_userdata( $abus_user->ID );
 											
 						/* build menu url */
-						$mdw_full_menu_url = $user_switching->switch_to_url( $mdw_user ).'&redirect_to='.mdw_current_url();
+						$abus_full_menu_url = $user_switching->switch_to_url( $abus_user ).'&redirect_to='.abus_current_url();
 						
 						/* build menu id for each user */
-						$mdw_menu_id = sanitize_key( $mdw_user_info->first_name . '-' . $mdw_user_info->last_name );
+						$abus_menu_id = sanitize_key( $abus_user_info->first_name . '-' . $abus_user_info->last_name );
 						
 						/* add admin bar menu to create each users switch to link */
 						$wp_admin_bar->add_menu( array(
-							'id'    => $mdw_menu_id,
-							'parent' => 'mdw_switch_to_user',
-							'title' => $mdw_user_info->display_name,
-							'href'  => $mdw_full_menu_url,
+							'id'    => $abus_menu_id,
+							'parent' => 'abus_switch_to_user',
+							'title' => $abus_user_info->display_name,
+							'href'  => $abus_full_menu_url,
 						) );
 						
 					} // 
@@ -139,13 +139,13 @@ function mdw_user_switching_adminbar() {
 			if( $user_switching->get_old_user() ) {
 				
 				/* build the switch back url */
-				$mdw_switch_back_url = $user_switching->switch_back_url( $user_switching->get_old_user() );
+				$abus_switch_back_url = $user_switching->switch_back_url( $user_switching->get_old_user() );
 				
 				/* we are logged in throught swtiching so add admin bar menu to create the switch back link */
 				$wp_admin_bar->add_menu( array(
 					'id'    => 'switch_back',
 					'title' => 'Switch Back',
-					'href'   => add_query_arg( array( 'redirect_to' => esc_url( mdw_current_url() ) ), $mdw_switch_back_url )
+					'href'   => add_query_arg( array( 'redirect_to' => esc_url( abus_current_url() ) ), $abus_switch_back_url )
 				) );
 				
 			} // end if old user present
@@ -157,4 +157,4 @@ function mdw_user_switching_adminbar() {
 }
 
 /* hook our customisations of the admin bar function into wordpress */
-add_action('wp_before_admin_bar_render', 'mdw_user_switching_adminbar', 0);
+add_action('wp_before_admin_bar_render', 'abus_user_switching_adminbar', 0);
