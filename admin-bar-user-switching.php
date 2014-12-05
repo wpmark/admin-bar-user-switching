09f7ea84e4df116fea966b49832b1787b1f5502d
Mark Wilkinson <mark@markwilkinson.me>
2014-12-05 15:48:21 +0000

    Bump Version Number

--- admin-bar-user-switching.php
+++ admin-bar-user-switching.php
@@ -5,7 +5,7 @@ Plugin URI: http://markwilkinson.me
 Description: Building upon the <a href="http://wordpress.org/extend/plugins/user-switching/">User Switching plugin</a> by John Blackbourn this plugin adds a dropdown list of users in the WordPress admin bar with a link to switch to that user, then providing a switch back link in the admin bar too.
 Author: Mark Wilkinson
 Author URI: http://markwilkinson.me
-Version: 0.1
+Version: 0.2
 */
 
 /******************************************************************************************
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://markwilkinson.me/saythanks/
 Tags: users, user switching
 Requires at least: 3.1
 Tested up to: 4.0.1
-Stable tag: 0.1
+Stable tag: 0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
